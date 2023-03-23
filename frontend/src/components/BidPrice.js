764a1b594e93d0f401c849944f32b4d08d87826b
amitp2139 <72243227+amitp2139@users.noreply.github.com>
2023-03-23 16:42:09 +0530

    added array

--- frontend/src/components/BidPrice.js
+++ frontend/src/components/BidPrice.js
@@ -3,20 +3,22 @@ import axios from 'axios';
 
 function BidPrice() {
   const [latestBidPrice, setLatestBidPrice] = useState('');
+  const [bidArray, setBidArray] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get('http://localhost:3001/letestBidprice');
         setLatestBidPrice(response.data.letestBidprice);
+        setBidArray(response.data.bidArray);
       } catch (error) {
         console.error(error);
       }
     };
-    // const interval = setInterval(() => {
+    const interval = setInterval(() => {
       fetchData();
-    // }, 1000);
-    // return () => clearInterval(interval);
+     }, 1000);
+    return () => clearInterval(interval);
   });
 
   return (
@@ -24,6 +26,15 @@ function BidPrice() {
         <h3>Latest Price From CoinMarketCap</h3>
         <h3>Connection Status:<b>Connection Stabalished</b></h3>
       <h3>Latest Bid Price: {latestBidPrice}</h3>
+      <div style={{ textAlign: "center", paddingTop: '150px' }}>
+          {bidArray.length > 0 && (
+            <ul style={{ listStyle: 'none', padding: 0 }}>
+              {bidArray.map((bid) => (
+                <li>{bid}</li>
+              ))}
+            </ul>
+          )}
+        </div>
     </div>
   );
 }
